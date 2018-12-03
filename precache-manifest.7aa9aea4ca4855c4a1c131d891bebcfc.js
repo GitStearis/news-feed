348654a0a99b1014d037197a8d1019ef710e82f0
GitStearis <egor.puisha@gmail.com>
2018-12-04 00:50:02 +0300

    Updates

---
+++ precache-manifest.7aa9aea4ca4855c4a1c131d891bebcfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a25cb2c34010209f91a",
+    "url": "/react-news-feed/static/css/main.ba5c4382.chunk.css"
+  },
+  {
+    "revision": "9a25cb2c34010209f91a",
+    "url": "/react-news-feed/static/js/main.9a25cb2c.chunk.js"
+  },
+  {
+    "revision": "7fadcd591809549facdb",
+    "url": "/react-news-feed/static/js/1.7fadcd59.chunk.js"
+  },
+  {
+    "revision": "2a33ceb29511a08d2085",
+    "url": "/react-news-feed/static/js/runtime~main.2a33ceb2.js"
+  },
+  {
+    "revision": "49455cb7450e5e6eff054477bfe4c13e",
+    "url": "/react-news-feed/index.html"
+  }
+];
